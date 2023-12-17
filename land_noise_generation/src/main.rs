a24ff16c6180a405551d3765a1ee849729f893f8
Jake <jakeor45@gmail.com>
2023-12-16 16:19:12 -0800

    adding in functionality to draw lines. Testing in other repo atm

--- land_noise_generation/src/main.rs
+++ land_noise_generation/src/main.rs
@@ -4,7 +4,14 @@ use rand::Rng;
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins)
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                title: "I am a window!".into(),
+                resolution: (1400., 800.).into(),
+                ..default()
+            }),
+            ..default()
+        }))
         .add_systems(Startup, setup)
         .run();
 }
@@ -14,9 +21,9 @@ fn setup(
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<ColorMaterial>>
 ) {
-    let rez: i64 = 15;
-    let cols: i64 = 1400 / rez;
-    let rows = 800 / rez;
+    let rez: i64 = 10;
+    let cols: i64 = (1400 / rez) + 1;
+    let rows = (800 / rez) + 1;
     let total_col_row = cols * rows;
     let mut column_major = Vec::new();
 
@@ -39,14 +46,51 @@ fn setup(
             commands.spawn(MaterialMesh2dBundle {
                 mesh: meshes.add(shape::Circle::new(2.0).into()).into(),
                 material: materials.add(Color::rgba_linear(color_value * 255.0, color_value * 255.0, color_value * 255.0, color_value).into()),
-                transform: Transform::from_translation(Vec3::new(-x_coord, y_coord - 50.0, 0.)),
+                transform: Transform::from_translation(Vec3::new(cols as f32 - x_coord, rows as f32 - y_coord, 0.)),
                 ..default()
             });
         }
+    }
+
+    for i in 1..rows{
+        for j in 1..cols {
+            let x = i * rez;
+            let y = j * rez;
+            let point_a = pointVector {
+                x_coord: (x + rez) as f32 * 0.5,
+                y_coord: y as f32
+            };
+            let point_b = pointVector {
+                x_coord: (x + rez) as f32,
+                y_coord: (y + rez) as f32 * 0.5
+            };
+            let point_c = pointVector {
+                x_coord: (x + rez) as f32 * 0.5,
+                y_coord: (y + rez) as f32,
+            };
+            let point_d = pointVector {
+                x_coord: x as f32,
+                y_coord: (y + rez) as f32 * 0.5
+            };
+            let length_a_b = ((point_a.x_coord - point_b.x_coord).powi(2) + (point_a.y_coord - point_b.y_coord).powi(2)).sqrt().sqrt();
+            let length_c_d = ((point_a.x_coord - point_b.y_coord).powi(2) + (point_b.x_coord - point_b.y_coord).powi(2)).sqrt().sqrt();
+            // commands.spawn(MaterialMesh2dBundle {
+            //     mesh: meshes.add(   Mesh::from(shape::Quad { size: Vec2::new(length_a_b, 1.0), flip: false})).into(),
+            //     material: materials.add(Color::rgb(255.0, 255.0, 255.0).into()),
+            //     transform: Transform::from_xyz(),
+            //     ..default()
+            // });
         }
+    }
        
     // for _ in 0..vec.capacity() {
     //     vec.push(rand::random());
     // };  
     
+}
+
+
+struct pointVector {
+    x_coord: f32,
+    y_coord: f32 
 }
