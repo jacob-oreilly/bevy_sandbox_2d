d65cec5777c3b0e91b157c72fe6f483452c94440
Jake <jakeor45@gmail.com>
2024-01-09 22:08:36 -0800

    Adding in a basic player that does nothing yet.

--- rand_race/src/main.rs
+++ rand_race/src/main.rs
@@ -2,10 +2,11 @@ use bevy::prelude::*;
 
 mod systems;
 use systems::*;
+pub mod components;
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        .add_systems(Startup, (setup, spawn_camera))
+        .add_systems(Startup, (setup, spawn_camera, spawn_entities))
         .run();
 }
--- rand_race/src/systems.rs
+++ rand_race/src/systems.rs
@@ -1,4 +1,5 @@
-use bevy::{prelude::*, window::PrimaryWindow};
+use bevy::{prelude::*, window::PrimaryWindow, sprite::MaterialMesh2dBundle};
+use super::components::Player;
 
 pub fn spawn_camera(mut commands: Commands, window_query: Query<&Window, With<PrimaryWindow>>) {
     let window = window_query.get_single().unwrap();
@@ -11,4 +12,27 @@ pub fn spawn_camera(mut commands: Commands, window_query: Query<&Window, With<Pr
 
 pub fn setup() {
     println!("Random Race!");
+}
+
+pub fn spawn_entities(mut commands: Commands, mut materials: ResMut<Assets<ColorMaterial>>, mut meshes: ResMut<Assets<Mesh>>, window_query: Query<&Window, With<PrimaryWindow>>){
+    let window = window_query.get_single().unwrap();
+    commands.spawn(
+        (
+            MaterialMesh2dBundle {
+                mesh: meshes.add(shape::Circle::new(10.0).into()).into(),
+                material: materials.add(
+                    Color::rgba_linear(
+                        255.0,
+                        255.0,
+                        255.0,
+                        1.0,
+                    )
+                    .into(),
+                ),
+                transform: Transform::from_xyz(window.width() / 2.0 , window.height() / 2.0, 0.0),
+                ..default()
+            },
+            Player {},
+        )
+    );
 }
