ae32704dc63004d0af57ce813a70ed15081ee313
Jake <jakeor45@gmail.com>
2024-02-03 21:15:52 -0800

    Player rotation is working correctly now.

--- fl_game/src/systems.rs
+++ fl_game/src/systems.rs
@@ -1,6 +1,4 @@
-use std::process::CommandArgs;
-
-use bevy::{prelude::*, render::{mesh::Indices, render_resource::PrimitiveTopology}, sprite::MaterialMesh2dBundle, window::PrimaryWindow};
+use bevy::{prelude::*, sprite::MaterialMesh2dBundle, window::PrimaryWindow};
 
 use crate::components::{self, Tourch};
 use components::Player;
@@ -48,52 +46,46 @@ pub fn spawn_player(
 }
 
 pub fn player_movement(
-    mut commands: Commands,
     keys: Res<Input<KeyCode>>,
     mut player_query: Query<(&mut Transform, &mut Player),With<Player>>,
     time: Res<Time>
 ) {
 
-    if let Ok((mut transform,  mut player)) = player_query.get_single_mut() {
+    if let Ok((mut transform,  player)) = player_query.get_single_mut() {
         let mut direction = Vec3::new(0.0, 0.0, 0.0);
-        let mut rotation: f32 = -1.0;
         if keys.pressed(KeyCode::Left) || keys.pressed(KeyCode::A) {
             direction += Vec3::new(-1.0, 0.0, 0.0);
-            rotation = 180.0;
         }
         if keys.pressed(KeyCode::Right) || keys.pressed(KeyCode::D) {
             direction += Vec3::new(1.0, 0.0, 0.0);
-            rotation = 0.0;
         }
         if keys.pressed(KeyCode::Up) || keys.pressed(KeyCode::W) {
             direction += Vec3::new(0.0, 1.0, 0.0);
-            rotation = 90.0;
         }
         if keys.pressed(KeyCode::Down) || keys.pressed(KeyCode::S) {
             direction += Vec3::new(0.0, -1.0, 0.0);
-            rotation = -90.0;
         }
 
         if direction.length() > 0.0 {
             direction = direction.normalize();
+            let current_direction = transform.translation.xy();
             transform.translation += direction * player.movement_speed * time.delta_seconds();
-        }
-        if rotation > -1.0 && player.player_rotation != rotation {
-            player.player_rotation = rotation;
-            transform.rotate_z(player.player_rotation.to_radians());
+            let to_forward = (transform.translation.xy() - current_direction).normalize();
+            let rotate_forward = Quat::from_rotation_arc(Vec3::X, to_forward.extend(0.0));
+            transform.rotation = rotate_forward;
         }
         
         
     }
 }
 
-pub fn spawn_walls(
-    commands: Commands,
-    meshes: ResMut<Assets<Mesh>>,
-    materials: ResMut<Assets<ColorMaterial>>
-) {
+// pub fn spawn_walls(
+//     commands: Commands,
+//     meshes: ResMut<Assets<Mesh>>,
+//     materials: ResMut<Assets<ColorMaterial>>
+// ) {
 
-}
+// }
 
 
 // pub fn spawn_tourch(
