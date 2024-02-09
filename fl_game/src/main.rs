a9fc0c052baa427fc47e19d10290ed4a0618df0c
Jake <jakeor45@gmail.com>
2024-02-08 17:49:46 -0800

    Working on adding the ray casting functionality for the tourch light

--- fl_game/src/main.rs
+++ fl_game/src/main.rs
@@ -9,5 +9,6 @@ fn main() {
         .insert_resource(Time::<Fixed>::from_hz(60.0))
         .add_systems(Startup, (setup, spawn_player, spawn_walls))
         .add_systems(FixedUpdate, player_movement)
+        .add_systems(Update, tourch_light_update)
         .run()
 }
