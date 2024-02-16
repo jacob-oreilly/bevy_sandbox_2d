b044916f83ce94e06e71f9fec2629b0d69a3e440
Jake <jakeor45@gmail.com>
2024-02-15 21:15:01 -0800

    Working on adding gravity

--- gravity_2d/src/main.rs
+++ gravity_2d/src/main.rs
@@ -5,10 +5,14 @@ pub struct MainCamera;
 
 #[derive(Component)]
 pub struct Player;
+
+#[derive(Component)]
+pub struct Floor;
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_systems(Startup, (spawn_camera, spawn_player, spawn_floor))
+        .add_systems(Update, update_player)
         .run();
 }
 
@@ -42,6 +46,23 @@ fn spawn_player(
     );
 }
 
+fn update_player(
+    mut player_query: Query<&mut Transform,With<Player>>,
+    time: Res<Time>
+) {
+    let acceleration = -9.8 * 150.0;
+    let player_mass = 25.0;
+    let mut velocity = 0.0;
+    let mut position = 0.0;
+    let dt = time.delta_seconds();
+    if let Ok(mut transform) = player_query.get_single_mut() {
+        velocity += acceleration * dt;
+        position += velocity * dt;
+        transform.translation.y += position;
+        println!("player translation: {:?}", transform.translation);
+    }
+}
+
 fn spawn_floor(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
@@ -58,7 +79,7 @@ fn spawn_floor(
                 transform: Transform::from_translation(Vec3::new(window.width() / 2.0, (window.height() / 2.0) - 200.0, 0.0)),
                 ..default()
             },
-            Player{},
+            Floor{},
         )
     );
 }
